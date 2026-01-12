3deb811261952a25390be0accf76697e247c3e47
CHO SANGWON <81066249+sangwon5579@users.noreply.github.com>
2026-01-12 20:44:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = 0
+    for k,v in dic.items():
+        s = s.replace(k,v)
+    answer = int(s)
+    return answer
+
+dic = {
+    "zero":"0",
+    "one":"1",
+    "two":"2",
+    "three":"3",
+    "four":"4",
+    "five":"5",
+    "six":"6",
+    "seven":"7",
+    "eight":"8",
+    "nine":"9"
+}
