dcb113dc6bfb121e3b9ea87e8109cf096bd0147e
CHO SANGWON <81066249+sangwon5579@users.noreply.github.com>
2026-01-12 15:01:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    answer = 0
+    if(num == 1):
+        answer = 0
+    while(answer < 500):
+        if(num == 1):
+            return answer
+        else:
+            answer += 1
+            if(num % 2 == 0):
+                num = num / 2
+            else:
+                num = num* 3 + 1
+    if(answer == 500):
+        answer = -1
+    return answer
