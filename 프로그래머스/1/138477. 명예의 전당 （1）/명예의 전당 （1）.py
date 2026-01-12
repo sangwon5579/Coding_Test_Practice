37332b4f7446a90ed5f3afe0d0a767e458985084
CHO SANGWON <81066249+sangwon5579@users.noreply.github.com>
2026-01-12 21:47:58 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.52 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,24 +1,41 @@
+# def solution(k, score):
+#     answer = []
+#     k_list = []
+#     if(k > len(score)):
+#         for i in range(len(score)):
+#             k_list.append(score[i])
+#             k_list.sort()
+#             answer.append(k_list[0])
+#         return answer
+#     else:        
+#         for i in range(k):
+#             k_list.append(score[i])
+#             k_list.sort()
+#             answer.append(k_list[0])
+#         leng = len(score) - k
+#         for i in range(leng):      
+#             if(score[i+k] > k_list[0]):
+#                 k_list.pop(0)
+#                 k_list.append(score[i+k])
+#                 k_list.sort()
+#                 answer.append(k_list[0])
+#             else:
+#                 answer.append(k_list[0])
+#         return answer
+    
+    
+import heapq
+
 def solution(k, score):
     answer = []
-    k_list = []
-    if(k > len(score)):
-        for i in range(len(score)):
-            k_list.append(score[i])
-            k_list.sort()
-            answer.append(k_list[0])
-        return answer
-    else:        
-        for i in range(k):
-            k_list.append(score[i])
-            k_list.sort()
-            answer.append(k_list[0])
-        leng = len(score) - k
-        for i in range(leng):      
-            if(score[i+k] > k_list[0]):
-                k_list.pop(0)
-                k_list.append(score[i+k])
-                k_list.sort()
-                answer.append(k_list[0])
-            else:
-                answer.append(k_list[0])
-        return answer
+    heap = []
+
+    for s in score:
+        heapq.heappush(heap, s)
+
+        if len(heap) > k:
+            heapq.heappop(heap)
+
+        answer.append(heap[0])
+
+    return answer
